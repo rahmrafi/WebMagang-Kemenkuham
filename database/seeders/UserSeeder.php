427c724af0537e0aa35add656606b7a9f252f9d2
unknown <rahmrafi>
2026-07-07 09:16:39 +0700

    Membuat seeder untuk admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create an admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('password@123'),
+            'is_admin' => true,
+        ]);
+    }
+}
